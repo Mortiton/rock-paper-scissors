ea9b4018ae44b27f7415fe09576e5d4b0de2479e
Zoe <159435044+Mortiton@users.noreply.github.com>
2024-03-10 07:48:20 +0000

    Add promise to the javascript file along with listener
    Added a promise to the javascript file to be able to generate a button
    only after the text has finished being typed.
    
    DOMContentLoaded event listener added to ensure the DOM content is DOMContentLoaded
    before manipulating the nodes.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //An array which contains the computers choices in the game
 const gameChoices = ["rock", "paper", "scissors"];
 
-
 //Determine the random choice by selecting a number between 0 and 2. This refers to an index in the array
 function getComputerChoice() {
   let computerChoice = gameChoices[Math.floor(Math.random() * 3)];
@@ -17,44 +16,64 @@ function getPlayerChoice() {
 }
 
 //determine the outcome based on computer and player choices
-function whoWins () {
-    let computerChoice = getComputerChoice();
-    // console.log(computerChoice);
-    let playerChoice = getPlayerChoice();
-    // console.log(playerChoice);
-    
-    //Player Wins
-    if ((computerChoice == 'rock' && playerChoice == 'paper')
-    || (computerChoice == 'paper' && playerChoice == 'scissors') 
-    || (computerChoice == 'scissors' && playerChoice == 'rock')) {
-        return console.log(`Computer chose ${computerChoice}.` + ' You win! :)');
-    }
-    //a tie
-    else if (computerChoice == playerChoice) {
-        return console.log(`Computer chose ${computerChoice}.` + ' Its a tie! :/');
-    }
-    else {
-        return console.log(`Computer chose ${computerChoice}.` + ' You lose! :(');
-    }
+function whoWins() {
+  let computerChoice = getComputerChoice();
+  let playerChoice = getPlayerChoice();
+  //Player Wins
+  if (
+    (computerChoice == "rock" && playerChoice == "paper") ||
+    (computerChoice == "paper" && playerChoice == "scissors") ||
+    (computerChoice == "scissors" && playerChoice == "rock")
+  ) {
+    return console.log(`Computer chose ${computerChoice}.` + " You win! :)");
+  }
+  //a tie
+  else if (computerChoice == playerChoice) {
+    return console.log(`Computer chose ${computerChoice}.` + " Its a tie! :/");
+  //Player loses
+  } else {
+    return console.log(`Computer chose ${computerChoice}.` + " You lose! :(");
+  }
 }
 
 function playGame() {
   whoWins();
 }
 
-//typing effect
+const typedText = document.querySelector("#typed-text");
 
-let charIndex = 0;
-
-const speed = 50;
-
+//typing effect with a promise. This is so I can ensure the text is displayed before a button is created
 function typeWriter(text) {
-  if (charIndex < text.length) {
-    const typedText = document.querySelector("#typed-text");
-    typedText.textContent += text.charAt(charIndex)
-    charIndex++;
-    setTimeout(() => typeWriter(text), speed);
-  }
+  return new Promise((resolve, reject) => {
+    let charIndex = 0;
+    const speed = 50;
+
+    function type() {
+      if (charIndex < text.length) {
+        typedText.textContent += text.charAt(charIndex);
+        charIndex++;
+        setTimeout(type, speed);
+      } else {
+        resolve();
+      }
+    }
+
+    type();
+  });
 }
 
-typeWriter("Do you want to play a game?")
+//starting text
+document.addEventListener('DOMContentLoaded', () => {
+typeWriter("Do you want to play a game?").then(() => {
+  setTimeout(() => {
+    //select the button
+    const button = document.querySelector("#play-btn");
+    button.textContent = "Test";
+    const gameDiv = document.querySelector("#game-container");
+    gameDiv.appendChild(button);
+
+    typedText.focus();
+  }, 1000); //delay the button display by 1 second
+});
+});
+//Create a button
